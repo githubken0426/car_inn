ddac94fe9d5314bd4b2ebcfef25b655089f46519
Administrator <Administrator@10.0.0.118>
2018-02-07 08:55:35 +0800

    2018-2-7

--- src/inn/shopping/api/pay/alipay/config/AlipayConfig.java
+++ src/inn/shopping/api/pay/alipay/config/AlipayConfig.java
@@ -14,20 +14,38 @@ package inn.shopping.api.pay.alipay.config;
 public class AlipayConfig {
 	// ↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 	// 合作身份者ID，签约账号，以2088开头由16位纯数字组成的字符串，查看地址：
-	//https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
+	// https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 	public static String partner = "2088921700888610";
 	/**
 	 * 商户的私钥,需要PKCS8格式，RSA公私钥生成：
 	 * https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
 	 */
-	public static String private_key = "MIIEugIBADANBgkqhkiG9w0BAQEFAASCBKQwggSgAgEAAoIBAQCD87JBraUgKstQZhyT7BzdypYirqYZnaNPhNeuqjvtAwY9Ve2SM3wnRNbV8RVp+0jWUE0UqQT8GVhQ4uVrZhD/2DI734Sn1+O7dH/Ov/oMxKGeTguLVIp8JEYdAxiFfOY2SX5akssnL11LRcVYordL5W6aeJXcGVQMblgXZI2VzYeoGusYotXSdjLytZ499d79EzH9/diywb9vq1P2jTYFyRzeRq47LLkibpcUzOZw5AxCfaSEfkNlUlo18P8/CPr4g5VAEeg57tI4sIs78g6Nbe1xHI/QeSshHyqkmjr7EwqeQc0+CYdHcm4+fIchEL2CWsq7gIilCzBO2BsB79YVAgMBAAECgf8g/MuTn9WoNQqfDugWEvEkgYEHKqJbxQ0O1CcXg3d3yUnGC7VY14YySwNqeNrho/CRo4GhqJ+WZr+teQvhzaJyU5o7XaXMUCyesIFDLrtUddPxv5EjQ/9bCbZ9ikiXFqJ27+imvAwNv8mTB95OSl+ImdMMyxir7akVcoCTWJAM4vjwZiOAPOW/GkUzDlMpdU377mLnPnXPRMBvF/bxjCyojhzAqbK5UPVOhSRLTr3xnvYJTrcGdh6JYxCDnpJQSyjmdrD9aZf6PFnz1jsTnN582iUcVeGYHSxAWTVAGv5snA5XAh02yD2m98LGp5GEpmnp53GQeDIs2qtJYoxNQwECgYEAxBIuGNafP/MtsFq6OpMmx4CVpkBAF0J9Nbyt5Yr919g/2ff9eP7dpRE/1Ar5rAOOUQ23QpdKGnzDEQFVlL9BtOl7X4OOyWNBe1YbGrxUFONAGSIo7l4D16yvnrK7WEMEZb4bR467omjzIQMSgu3ngnDjm29SYD/9fdBuPaiYU8MCgYEArEhwgqiB/e9IcWKtSsutObeMEGc4TTlaSuOb1kWC5VqeLasvZ7BAWZNE98gHFWviHJOzYJh/lOqI4ZnfEsRRYUiJ1+wYyUdcBiHUtNg+MUNLTHUHqbp6Qf5ZtYyd6JmkuRPktMu4EyGppkHAo4JJgNAxwWyHHdDaUe4yhpoaSUcCgYBGXb/tahj8+mkLQYRUQTtJnfwXDjfyIQqOnohKOPeg3xsLPtTY957T/rzJfKbd9Oi+bg22+FS/7MaU88Wi/IC4TJLaVHyKCfUWLnDLx4rldCks2/WUlmkKng7pBXv20RZi7TcdFzx/rOqUjmW2V2sehSUqQ2ls4Yo3/PWTa04/wQKBgDh9dC7Tc/RArn9HPM0mpcEkFxAV2Fcx9BJUOXfzyOzNXspYtC2KlD8K0/VvKCt0+WonYAzPVB4PHRzkKEnuB0Uf1Nrw9luhQ2JbW3SE5zed+iWyxfV4SLg031NxLSHkpydsh3RkzBGx2v/B1ZmHkfRjz7y9ijxTjBKLSRPrmaznAoGAA3vDMApWStc/6QRK7bREXVrkGTXfIMi2aNXYcdZTCsoHNAiQQ892YboaF1fmHy7yntSW8hMOynb9Dw4hf0g2ARbBlt6Al8opYCJDEp1laWUGsfpFTtMiHaLXwCSt+Qq8m9VyH1pQwIU/WtBNILWL/feAvAXE/IqGSUbfhF0ALSg=";
+	public static String private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCemaZjOrrlXi"
+			+ "DWDbtog5eEmN98N8pGrk1SLzV3mAm8raym4jx7l2xzPMa1HRlegj5ZLF+rkSIDi++nIzudFaHCsvil1vwi6qKqa"
+			+ "qT0az7UCbbgnPvYAqQkwFRwH2xDLQht+55ajrl4F39dLV8lgYZcs1Svit00FWxU1KlVrVDiKQDETwUfHP8uv7GD"
+			+ "s2F5hq9hODKa7+hNl28LoxJjO8KxVrC7l4fFZKMWR/UTq2aXs/pDN/FpDtHDsCJWUqa86B3dZUdzl916zeyhdmT"
+			+ "DodNcC6rwV8By9UCmBYPqHhhP28hewW1FeIXZ3lPnsNzYPArSaXIAOMySCkZHtGQ1Vj0zAgMBAAECggEACcZAae"
+			+ "cps48ciPSpsyRBB0wAogAyOC7v1vvP2MIG/5jYhiR1Q8yuQ1/ggzL66EcwnoBMmR9FsGBEcdGLEABQStaK2nWobm"
+			+ "J1ifkul12wrpK2sZsbLlHjWdx8l7hoDK/O0DBsrvoS1iLkl5J/Ral45Wer9XGidl5HoebFVOFslbOGX2aHNwnPZJ"
+			+ "uIaOv9PlIIMIhynQg7StRPX89Ps6Efr9zZ5QWTZ4MccmnJibDgdt8O8Okchf+HBJ0clgyfBxyiiYj0PbrfiUOwJM"
+			+ "mJL5kIhyhUmNSXC1l5a56ecYT3gbPQBVav3Q9PXi8Ubvs/iPWsruGpG3VowTxalwf0uamz4QKBgQDOjUmA90AhV7"
+			+ "xpS83knHvu3FTPj74aouDRgE5Dh2we8hGcUsfwFSLGx9A7PFvlQpNU/w5btKEffopC74AkVrsF/U7FfI+65txB68"
+			+ "tWRUMYiKufbqYIBGXL1xNenc+DFUwuAOlCKtWUbdJrTk2u2QR9kO1lugh0zNCZlgdGrc2qkQKBgQDEkZnKiYhqE+"
+			+ "16QuZNHf259HatbH7rMvJO8WVx8cCZGY4cd4ySGvW9yVm/1QCl63/76VUqAwTciXct27++T+MV1hsV2vJAW9YyUS"
+			+ "ojp54qz0OqVO9FttitVtWeqnd2cjCsnGvFtXfAtp5ooIGiOfV3iz1M6pGvIiLudc8aguElgwKBgGerGBJHhoN1op"
+			+ "YCuvMb60QGh/apQZPi3g2oZR+Vj5m0i0w3bCw+n3a/gO7KK6FUmdfWL+GtNhU1kLVpuZoDgdjOu0N5cjYveqsHas"
+			+ "YA+10L83+QrHpnoqP9bbZWZHl5LgQE5w4Tq6DUwgCji2/PcmaHX+9nwKjGx4feegZqY0hRAoGAKPQEzJO23hLkyT"
+			+ "Ii9TIoaJOsHhman7nvjc7xSboKXFtcpkWp/c6sICxRDIx+JBNaiMtmOOiEHMZvFBjSa/4M28HNxtHk4s+/YqBSXT"
+			+ "dQ1q5z9IrZhva/Ue3jd7kgRocn6O517a0PFBF4E2hNWdUldRCHZvz/+TLtgCz/RxC5g60CgYEAxgX9YV2fLuD4HG"
+			+ "dEAqTPaMttflmTkm5CziyVRv1eyz3WSKwMr7hARO4tzttk5hZv/0jcjLBGp6tt3IzCkD/2WlxvNIs/m1GqcyNUr1"
+			+ "1C5W6q3UI/x0gCnPXYTu1QYs9F5BV1mda6hq5CepKhSLDCxwl9KMO6zYOYuCEosYuaq4=";
 
 	// 支付宝的公钥，查看地址：https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 	public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlbk3s0Kl52cmj773Y2MPUpDFyhfs6G4Gfv1HO+3ALbyBtV//soer7yLEv9r0L4YoT154e4Wz9c6I6Tl+ZvPhF9p8XkGQLaOLMPvbANOErtwKEJ0Ts6g9zqISXlDb/dVF0fvkUm3FjqTUBHi3WNx6mEuIDC7xJaU78QW3XLZUYuq7cEtiQnJ2nQU+XGcbrzLt9JsPnB1o30h6A7IKgDTSLZezzCVQ6Aoj8K1aNicZL8NXaL7UW7+gIA0ePS0FKz9/CLYbXlALRRIRAtRVEPP6cqVfnDmgeSm+JdruVBgzwhavaobCAKdRzKtlR2/8g2I/oXBwKJa5Xg1uYwGK9yOLZQIDAQAB";
 	// 签约卖家支付宝账号
 	public static String seller_id = "m13941850400@163.com";
-	
-	public static String appid=" 2018011901977798";
+
+	public static String appid = " 2018011901977798";
 	// 签名方式
 	public static String sign_type = "RSA";
 
@@ -40,8 +58,8 @@ public class AlipayConfig {
 	// 接收通知的接口名
 	public static String service = "mobile.securitypay.pay";
 
-	//同步返回使用GET方式获取，异步通知使用POST方式获取；
-	//同步返回验证有1分钟超时，异步通知验证没有时间限制。
+	// 同步返回使用GET方式获取，异步通知使用POST方式获取；
+	// 同步返回验证有1分钟超时，异步通知验证没有时间限制。
 	// 服务器异步通知页面路径 需http://格式的完整路径，不能加?id=123这类自定义参数，必须外网可以正常访问
 	public static String notify_url = "http://114.215.71.170/car_inn/v1/ali/notifyurl";
 
@@ -58,8 +76,7 @@ public class AlipayConfig {
 	public static String body = "车驿站订单";
 
 	// ↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
-	
-	
+
 	// 交易关闭
 	public static final String TRADE_CLOSED = "TRADE_CLOSED";
 	public static final String TRADE_FINISHED = "TRADE_FINISHED"; // 交易完结 0
