43c8815700e16c52c1aa9b0cfbb68730ce9d5453
ken <ken@LAPTOP-1LAEVAMM>
2018-02-07 21:16:28 +0800

    2018-2-6

--- src/inn/shopping/api/pay/alipay/config/AlipayConfig.java
+++ src/inn/shopping/api/pay/alipay/config/AlipayConfig.java
@@ -20,26 +20,33 @@ public class AlipayConfig {
 	 * 商户的私钥,需要PKCS8格式，RSA公私钥生成：
 	 * https://doc.open.alipay.com/doc2/detail.htm?spm=a219a.7629140.0.0.nBDxfy&treeId=58&articleId=103242&docType=1
 	 */
-	public static String private_key = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCemaZjOrrlXi"
-			+ "DWDbtog5eEmN98N8pGrk1SLzV3mAm8raym4jx7l2xzPMa1HRlegj5ZLF+rkSIDi++nIzudFaHCsvil1vwi6qKqa"
-			+ "qT0az7UCbbgnPvYAqQkwFRwH2xDLQht+55ajrl4F39dLV8lgYZcs1Svit00FWxU1KlVrVDiKQDETwUfHP8uv7GD"
-			+ "s2F5hq9hODKa7+hNl28LoxJjO8KxVrC7l4fFZKMWR/UTq2aXs/pDN/FpDtHDsCJWUqa86B3dZUdzl916zeyhdmT"
-			+ "DodNcC6rwV8By9UCmBYPqHhhP28hewW1FeIXZ3lPnsNzYPArSaXIAOMySCkZHtGQ1Vj0zAgMBAAECggEACcZAae"
-			+ "cps48ciPSpsyRBB0wAogAyOC7v1vvP2MIG/5jYhiR1Q8yuQ1/ggzL66EcwnoBMmR9FsGBEcdGLEABQStaK2nWobm"
-			+ "J1ifkul12wrpK2sZsbLlHjWdx8l7hoDK/O0DBsrvoS1iLkl5J/Ral45Wer9XGidl5HoebFVOFslbOGX2aHNwnPZJ"
-			+ "uIaOv9PlIIMIhynQg7StRPX89Ps6Efr9zZ5QWTZ4MccmnJibDgdt8O8Okchf+HBJ0clgyfBxyiiYj0PbrfiUOwJM"
-			+ "mJL5kIhyhUmNSXC1l5a56ecYT3gbPQBVav3Q9PXi8Ubvs/iPWsruGpG3VowTxalwf0uamz4QKBgQDOjUmA90AhV7"
-			+ "xpS83knHvu3FTPj74aouDRgE5Dh2we8hGcUsfwFSLGx9A7PFvlQpNU/w5btKEffopC74AkVrsF/U7FfI+65txB68"
-			+ "tWRUMYiKufbqYIBGXL1xNenc+DFUwuAOlCKtWUbdJrTk2u2QR9kO1lugh0zNCZlgdGrc2qkQKBgQDEkZnKiYhqE+"
-			+ "16QuZNHf259HatbH7rMvJO8WVx8cCZGY4cd4ySGvW9yVm/1QCl63/76VUqAwTciXct27++T+MV1hsV2vJAW9YyUS"
-			+ "ojp54qz0OqVO9FttitVtWeqnd2cjCsnGvFtXfAtp5ooIGiOfV3iz1M6pGvIiLudc8aguElgwKBgGerGBJHhoN1op"
-			+ "YCuvMb60QGh/apQZPi3g2oZR+Vj5m0i0w3bCw+n3a/gO7KK6FUmdfWL+GtNhU1kLVpuZoDgdjOu0N5cjYveqsHas"
-			+ "YA+10L83+QrHpnoqP9bbZWZHl5LgQE5w4Tq6DUwgCji2/PcmaHX+9nwKjGx4feegZqY0hRAoGAKPQEzJO23hLkyT"
-			+ "Ii9TIoaJOsHhman7nvjc7xSboKXFtcpkWp/c6sICxRDIx+JBNaiMtmOOiEHMZvFBjSa/4M28HNxtHk4s+/YqBSXT"
-			+ "dQ1q5z9IrZhva/Ue3jd7kgRocn6O517a0PFBF4E2hNWdUldRCHZvz/+TLtgCz/RxC5g60CgYEAxgX9YV2fLuD4HG"
-			+ "dEAqTPaMttflmTkm5CziyVRv1eyz3WSKwMr7hARO4tzttk5hZv/0jcjLBGp6tt3IzCkD/2WlxvNIs/m1GqcyNUr1"
-			+ "1C5W6q3UI/x0gCnPXYTu1QYs9F5BV1mda6hq5CepKhSLDCxwl9KMO6zYOYuCEosYuaq4=";
-
+	public static String private_key ="MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCemaZjOrrlXiDW" + 
+			"Dbtog5eEmN98N8pGrk1SLzV3mAm8raym4jx7l2xzPMa1HRlegj5ZLF+rkSIDi++n" + 
+			"IzudFaHCsvil1vwi6qKqaqT0az7UCbbgnPvYAqQkwFRwH2xDLQht+55ajrl4F39d" + 
+			"LV8lgYZcs1Svit00FWxU1KlVrVDiKQDETwUfHP8uv7GDs2F5hq9hODKa7+hNl28L" + 
+			"oxJjO8KxVrC7l4fFZKMWR/UTq2aXs/pDN/FpDtHDsCJWUqa86B3dZUdzl916zeyh" + 
+			"dmTDodNcC6rwV8By9UCmBYPqHhhP28hewW1FeIXZ3lPnsNzYPArSaXIAOMySCkZH" + 
+			"tGQ1Vj0zAgMBAAECggEACcZAaecps48ciPSpsyRBB0wAogAyOC7v1vvP2MIG/5jY" + 
+			"hiR1Q8yuQ1/ggzL66EcwnoBMmR9FsGBEcdGLEABQStaK2nWobmJ1ifkul12wrpK2" + 
+			"sZsbLlHjWdx8l7hoDK/O0DBsrvoS1iLkl5J/Ral45Wer9XGidl5HoebFVOFslbOG" + 
+			"X2aHNwnPZJuIaOv9PlIIMIhynQg7StRPX89Ps6Efr9zZ5QWTZ4MccmnJibDgdt8O" + 
+			"8Okchf+HBJ0clgyfBxyiiYj0PbrfiUOwJMmJL5kIhyhUmNSXC1l5a56ecYT3gbPQ" + 
+			"BVav3Q9PXi8Ubvs/iPWsruGpG3VowTxalwf0uamz4QKBgQDOjUmA90AhV7xpS83k" + 
+			"nHvu3FTPj74aouDRgE5Dh2we8hGcUsfwFSLGx9A7PFvlQpNU/w5btKEffopC74Ak" + 
+			"VrsF/U7FfI+65txB68tWRUMYiKufbqYIBGXL1xNenc+DFUwuAOlCKtWUbdJrTk2u" + 
+			"2QR9kO1lugh0zNCZlgdGrc2qkQKBgQDEkZnKiYhqE+16QuZNHf259HatbH7rMvJO" + 
+			"8WVx8cCZGY4cd4ySGvW9yVm/1QCl63/76VUqAwTciXct27++T+MV1hsV2vJAW9Yy" + 
+			"USojp54qz0OqVO9FttitVtWeqnd2cjCsnGvFtXfAtp5ooIGiOfV3iz1M6pGvIiLu" + 
+			"dc8aguElgwKBgGerGBJHhoN1opYCuvMb60QGh/apQZPi3g2oZR+Vj5m0i0w3bCw+" + 
+			"n3a/gO7KK6FUmdfWL+GtNhU1kLVpuZoDgdjOu0N5cjYveqsHasYA+10L83+QrHpn" + 
+			"oqP9bbZWZHl5LgQE5w4Tq6DUwgCji2/PcmaHX+9nwKjGx4feegZqY0hRAoGAKPQE" + 
+			"zJO23hLkyTIi9TIoaJOsHhman7nvjc7xSboKXFtcpkWp/c6sICxRDIx+JBNaiMtm" + 
+			"OOiEHMZvFBjSa/4M28HNxtHk4s+/YqBSXTdQ1q5z9IrZhva/Ue3jd7kgRocn6O51" + 
+			"7a0PFBF4E2hNWdUldRCHZvz/+TLtgCz/RxC5g60CgYEAxgX9YV2fLuD4HGdEAqTP" + 
+			"aMttflmTkm5CziyVRv1eyz3WSKwMr7hARO4tzttk5hZv/0jcjLBGp6tnt3IzCkD/" + 
+			"2WlxvNIs/m1GqcyNUr11C5W6q3UI/x0gCnPXYTu1QYs9F5BV1mda6hq5CepKhSLD" + 
+			"Cxwl9KMO6zYOYuCEosYuaq4=";
+	
 	// 支付宝的公钥，查看地址：https://openhome.alipay.com/platform/keyManage.htm?keyType=partner
 	public static String alipay_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlbk3s0Kl52cmj773Y2MPUpDFyhfs6G4Gfv1HO+3ALbyBtV//soer7yLEv9r0L4YoT154e4Wz9c6I6Tl+ZvPhF9p8XkGQLaOLMPvbANOErtwKEJ0Ts6g9zqISXlDb/dVF0fvkUm3FjqTUBHi3WNx6mEuIDC7xJaU78QW3XLZUYuq7cEtiQnJ2nQU+XGcbrzLt9JsPnB1o30h6A7IKgDTSLZezzCVQ6Aoj8K1aNicZL8NXaL7UW7+gIA0ePS0FKz9/CLYbXlALRRIRAtRVEPP6cqVfnDmgeSm+JdruVBgzwhavaobCAKdRzKtlR2/8g2I/oXBwKJa5Xg1uYwGK9yOLZQIDAQAB";
 	// 签约卖家支付宝账号
