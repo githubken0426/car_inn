b9610f4e8ed3bf78e1cf85c5d1e2b21a8bb557ac
ken <ken@LAPTOP-1LAEVAMM>
2018-04-25 19:24:47 +0800

    2018-4-25

--- src/inn/shopping/api/utils/AliSMSUtils.java
+++ src/inn/shopping/api/utils/AliSMSUtils.java
@@ -14,9 +14,9 @@ import inn.shopping.api.InnApiConfig;
 public class AliSMSUtils {
 	public static void main(String[] args) {
 		String mobile = "18642690085";
-		//String mobile = "15840602341";
+		// String mobile = "15840602341";
 		String tempCode = InnApiConfig.MESSAGE_ORDER_TEMPLATE;
-		sendUserSelfMsg(mobile,"order_002","2018-04-25","2018-04-29","小波货站", tempCode);
+		sendUserSelfMsg(mobile, "order_002", "2018-04-25", "2018-04-29", "小波货站", tempCode);
 	}
 
 	public static void sendVerifyCodeMsg(String mobile, String tempCode, String validateCode) {
@@ -41,17 +41,24 @@ public class AliSMSUtils {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * 用户订单付款后,短信接收人：用户
+	 * 
 	 * @param mobile
-	 * @param orderNo 订单号
-	 * @param date 到达日期
-	 * @param serviceDate 服务日期
-	 * @param dealer 服务商名称
-	 * @param tempCode 短信模板
+	 * @param orderNo
+	 *            订单号
+	 * @param date
+	 *            到达日期
+	 * @param serviceDate
+	 *            服务日期
+	 * @param dealer
+	 *            服务商名称
+	 * @param tempCode
+	 *            短信模板
 	 */
-	public static void sendUserSelfMsg(String mobile,String orderNo,String date,
-				String serviceDate,String dealer, String tempCode) {
+	public static void sendUserSelfMsg(String mobile, String orderNo, String date, String serviceDate, String dealer,
+			String tempCode) {
 		try {
 			String signName = InnApiConfig.MESSAGE_SIGN;// 签名名称
 			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", InnApiConfig.MESSAGE_ACCESS_KEY,
@@ -61,7 +68,7 @@ public class AliSMSUtils {
 			SingleSendSmsRequest request = new SingleSendSmsRequest();
 			request.setSignName(signName);// 控制台创建的签名名称
 			request.setTemplateCode(tempCode);// 控制台创建的模板CODE
-			StringBuffer sb=new StringBuffer();
+			StringBuffer sb = new StringBuffer();
 			// 短信模板中的变量；数字需要转换为字符串；个人用户每个变量长度必须小于15个字符。
 			sb.append("{\"danhao\":\"");
 			sb.append(orderNo);
@@ -73,13 +80,81 @@ public class AliSMSUtils {
 			sb.append(serviceDate);
 			sb.append("\",\"product\":\"");
 			sb.append(signName);
-			sb.append( "\"}");
+			sb.append("\"}");
 			request.setParamString(sb.toString());
 			request.setRecNum(mobile);// 接收号码
 			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
 			System.out.println(httpResponse);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
+	}
+
+	/**
+	 * 服务商接订单（用户付款后）短信接收人：服务商
+	 * 
+	 * @param mobile
+	 * @param userName
+	 * @param orderNo
+	 * @param date
+	 * @param tempCode
+	 */
+	public static void sendDelaerMsg(String mobile, String userName, String orderNo, String date, String tempCode) {
+		try {
+			String signName = InnApiConfig.MESSAGE_SIGN;// 签名名称
+			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", InnApiConfig.MESSAGE_ACCESS_KEY,
+					InnApiConfig.MESSAGE_ACCESS_SECRET);
+			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
+			IAcsClient client = new DefaultAcsClient(profile);
+			SingleSendSmsRequest request = new SingleSendSmsRequest();
+			request.setSignName(signName);// 控制台创建的签名名称
+			request.setTemplateCode(tempCode);// 控制台创建的模板CODE
+			StringBuffer sb = new StringBuffer();
+			sb.append("{\"name\":\"");
+			sb.append(userName);
+			sb.append("\"danhao\":\"");
+			sb.append(orderNo);
+			sb.append("\",\"daohuo\":\"");
+			sb.append(date);
+			sb.append("\",\"product\":\"");
+			sb.append(signName);
+			sb.append("\"}");
+			request.setParamString(sb.toString());
+			request.setRecNum(mobile);// 接收号码
+			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
+			System.out.println(httpResponse);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void sendUserDealerMsg(String mobile, String orderNo, String serviceDate, String dealer,
+			String tempCode) {
+		try {
+			String signName = InnApiConfig.MESSAGE_SIGN;// 签名名称
+			IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", InnApiConfig.MESSAGE_ACCESS_KEY,
+					InnApiConfig.MESSAGE_ACCESS_SECRET);
+			DefaultProfile.addEndpoint("cn-hangzhou", "cn-hangzhou", "Sms", "sms.aliyuncs.com");
+			IAcsClient client = new DefaultAcsClient(profile);
+			SingleSendSmsRequest request = new SingleSendSmsRequest();
+			request.setSignName(signName);// 控制台创建的签名名称
+			request.setTemplateCode(tempCode);// 控制台创建的模板CODE
+			StringBuffer sb = new StringBuffer();
+			sb.append("{\"danhao\":\"");
+			sb.append(orderNo);
+			sb.append("\",\"fuwushang\":\"");
+			sb.append(dealer);
+			sb.append("\",\"fuwuriqi\":\"");
+			sb.append(serviceDate);
+			sb.append("\",\"product\":\"");
+			sb.append(signName);
+			sb.append("\"}");
+			request.setParamString(sb.toString());
+			request.setRecNum(mobile);// 接收号码
+			SingleSendSmsResponse httpResponse = client.getAcsResponse(request);
+			System.out.println(httpResponse);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
