bbdc655fb0427ec1d3d93f51cbfaa67eec2690aa
ken <ken@192.168.1.102>
2018-07-01 20:34:47 +0800

    20180701

--- src/inn/shopping/api/dao/ShopMapper.java
+++ src/inn/shopping/api/dao/ShopMapper.java
@@ -2,7 +2,6 @@ package inn.shopping.api.dao;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import inn.shopping.api.entity.Shop;
